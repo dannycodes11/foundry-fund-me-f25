a888410fb736c9cef88fbf4632f35073fab1b70e
Oluwapelumi <oluwapelumidaniel88@gmail.com>
2025-11-27 12:28:49 +0100

    Initial commit for foundry fund me project

--- src/FundMe.sol
+++ src/FundMe.sol
@@ -3,7 +3,8 @@
 pragma solidity 0.8.19;
 
 // 2. Imports
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
 import {PriceConverter} from "./PriceConverter.sol";
 
 // 3. Interfaces, Libraries, Contracts
